3ba7dd63c164cc7217d9a03d3850e99169eb0a20
Lango <vladdromero@gmail.com>
2022-07-18 15:17:05 -0400

    fix for netlify

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Landing.css';
 import logoLight from '../../assets/images/logos/logo-light.png';
 import logoDark from '../../assets/images/logos/logo-dark.png';
-const images = [{ logoLight }, { logoDark }];
+// const images = [{ logoLight }, { logoDark }];
 
 
 
